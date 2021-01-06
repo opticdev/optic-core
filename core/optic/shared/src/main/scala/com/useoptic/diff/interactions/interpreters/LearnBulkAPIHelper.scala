88d521bed4e07ca3ec6341bbe5387750216e2458
Aidan Cunniffe <aidan@useoptic.com>
2021-01-06 15:43:20 -0500

    made js bridge easier to work with

--- core/optic/shared/src/main/scala/com/useoptic/diff/interactions/interpreters/LearnBulkAPIHelper.scala
+++ core/optic/shared/src/main/scala/com/useoptic/diff/interactions/interpreters/LearnBulkAPIHelper.scala
@@ -3,18 +3,28 @@ package com.useoptic.diff.interactions.interpreters
 import com.useoptic.contexts.rfc.RfcState
 import com.useoptic.diff.helpers.{UndocumentedUrlHelpers, UndocumentedUrlIncrementalHelpers}
 import com.useoptic.diff.initial.{DistributionAwareShapeBuilder, ShapeBuildingStrategy, StreamingShapeBuilder}
+import com.useoptic.diff.interactions.interpreters.ExpectedHelper.{expectedForDiff, parseShapeTrail}
 import com.useoptic.diff.interactions.interpreters.LearnAPIHelper.ShapeBuilderMap
 import com.useoptic.diff.interactions.{BodyUtilities, ContentTypeHelpers}
 import com.useoptic.dsa.{OpticDomainIds, OpticIds}
 import com.useoptic.types.capture.HttpInteraction
+import io.circe.Json
 
 import scala.scalajs.js.annotation.{JSExport, JSExportAll}
 
+@JSExport
+@JSExportAll
 case class EndpointToLearn(pathId: String, method: String)
 
 @JSExport
 @JSExportAll
-class LearnBulkAPIHelper(rfcState: RfcState, endpoints: Vector[EndpointToLearn], opticIds: OpticDomainIds = OpticIds.generator) {
+class LearnBulkAPIHelper(rfcState: RfcState, json: Json, opticIds: OpticDomainIds = OpticIds.generator) {
+
+  import io.circe._, io.circe.parser._
+  import io.circe.generic.auto._
+  import io.circe.syntax._
+
+  val endpoints = json.as[Vector[EndpointToLearn]].right.get
 
   val shapeBuilderEndpointMaps: Map[EndpointToLearn, ShapeBuilderMap] = {
     endpoints.map(i => (i, LearnAPIHelper.newShapeBuilderMap(i.pathId, i.method, opticIds)))
@@ -43,6 +53,10 @@ class LearnBulkAPIHelper(rfcState: RfcState, endpoints: Vector[EndpointToLearn],
 
   def getResults(endpointToLearn: EndpointToLearn): ShapeBuilderMap = {
     shapeBuilderEndpointMaps(endpointToLearn)
+  }
+
+  def getResults(pathId: String, method: String): ShapeBuilderMap = {
+    shapeBuilderEndpointMaps.find(i => i._1.method == method && i._1.pathId == pathId).get._2
   }
 
 }
